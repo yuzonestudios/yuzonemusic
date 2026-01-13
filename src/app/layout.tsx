c392c04c5fac3842d85d76add18614fba02b1867
SMG Interactive <yuzonestudios@gmail.com>
2026-01-14 01:05:00 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <body>
                 <Providers>{children}</Providers>
             </body>
