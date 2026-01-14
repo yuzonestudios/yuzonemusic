fb86999afb8efdb1a5445bab931043fff93a557c
SMG Interactive <yuzonestudios@gmail.com>
2026-01-14 19:01:18 +0530

    mobile

---
+++ src/components/layout/MobileNav.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Home, Search, Library, Trophy, Settings } from "lucide-react";
+import styles from "./MobileNav.module.css";
+
+const navItems = [
+    {
+        href: "/dashboard",
+        label: "Home",
+        icon: <Home size={24} />,
+    },
+    {
+        href: "/search",
+        label: "Search",
+        icon: <Search size={24} />,
+    },
+    {
+        href: "/top",
+        label: "Top",
+        icon: <Trophy size={24} />,
+    },
+    {
+        href: "/library",
+        label: "Library",
+        icon: <Library size={24} />,
+    },
+    {
+        href: "/settings",
+        label: "Settings",
+        icon: <Settings size={24} />,
+    },
+];
+
+export default function MobileNav() {
+    const pathname = usePathname();
+
+    return (
+        <nav className={styles.mobileNav}>
+            {navItems.map((item) => (
+                <Link
+                    key={item.href}
+                    href={item.href}
+                    className={`${styles.navItem} ${pathname === item.href ? styles.active : ""}`}
+                >
+                    <span className={styles.icon}>{item.icon}</span>
+                    <span className={styles.label}>{item.label}</span>
+                </Link>
+            ))}
+        </nav>
+    );
+}
