000a3ce9d8c649b7f591a3788814c3d3b5aa19fd
Agnibha Mukherjee <yuzonestudios@gmail.com>
2026-01-15 12:54:55 +0530

    restore

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,14 +2,11 @@
 
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "@/context/ThemeContext";
-import { UTMTracker } from "@/components/UTMTracker";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     return (
         <SessionProvider>
-            <ThemeProvider>
-                <UTMTracker>{children}</UTMTracker>
-            </ThemeProvider>
+            <ThemeProvider>{children}</ThemeProvider>
         </SessionProvider>
     );
 }
--- src/components/UTMTracker.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useUTMTracking } from "@/hooks/useUTMTracking";
-
-export function UTMTracker({ children }: { children: React.ReactNode }) {
-    useUTMTracking();
-    return <>{children}</>;
-}
