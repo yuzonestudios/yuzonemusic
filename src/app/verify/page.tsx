d09bec9c5055cf38c739d0eb80234df194400780
Agnibha Mukherjee <yuzonestudios@gmail.com>
2026-02-06 19:25:54 +0530

    solvewed

--- src/app/verify/page.tsx
+++ src/app/verify/page.tsx
@@ -4,6 +4,7 @@ import connectDB from "@/lib/mongodb";
 import User from "@/models/User";
 
 export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
 
 export default async function VerifyPage({
     searchParams,
